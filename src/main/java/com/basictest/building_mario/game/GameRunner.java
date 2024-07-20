c9f2a39fb4729d394e3119fcbc6d0772cf10a17b
aryan4codes <av.rajpurkar@gmail.com>
2024-07-20 19:39:44 +0530

    fix: Fixed errors

--- src/main/java/com/basictest/building_mario/game/GameRunner.java
+++ src/main/java/com/basictest/building_mario/game/GameRunner.java
@@ -1,4 +1,13 @@
 package com.basictest.building_mario.game;
 
 public class GameRunner {
+    MarioGame game;
+
+    public GameRunner(MarioGame game) {
+        this.game = game;
+    }
+
+    public void run() {
+        System.out.println("Mario is running"+ game);
+    }
 }
